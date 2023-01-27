b59422b91dd44e5a57ea8c14577806b05f919996
Nelson Cabrera <80597467+onairhn7@users.noreply.github.com>
2023-01-26 20:31:46 -0600

    Agregar archivos de proyecto.

---
+++ Expresiones.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    internal class Expresiones
+    {
+
+        // Caracteristicas o propiedades de la clase 
+        public double num1 { get; set; }
+        public double num2 { get; set; }  
+
+        // aqui van los metodos de la clase exporesion 
+
+        public double Suma()
+        {
+            double varSuma = num1 + num2;
+
+            return varSuma; 
+
+        }
+
+        public double Resta()
+        {
+            double varResta = num1 - num2;
+
+            return varResta;
+
+        }
+
+        public double Multiplicacion()
+        {
+            double varMultiplicacion = num1 * num2;
+
+            return varMultiplicacion;
+
+        }
+
+        public double Division()
+        {
+            double varDivision = num1 / num2;
+
+            return varDivision;
+
+        }
+
+        public void NumMayor()
+        {
+            
+            
+
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Xml.XPath;
+using Tarea2;
+
+Expresiones Expresiones = new Expresiones();
+Expresiones.num1 = 20;
+Expresiones.num2 = 10;
+
+Expresiones Expresiones2 = new Expresiones();
+Expresiones.num1 = 8;
+Expresiones.num2 = 10;
+
+
+Console.WriteLine("Suma: " + Expresiones.Suma);
